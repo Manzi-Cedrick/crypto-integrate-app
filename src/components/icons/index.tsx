da65920838d72b6fc430bb8b69807c69f51e0c85
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-10-03 22:41:51 +0200

    [core]: project init configuration

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,14 @@
+export const MenuSideBarIcon = () => {
+    return (
+        <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4.66667 21H9.33333C9.975 21 10.5 20.475 10.5 19.8333C10.5 19.1917 9.975 18.6667 9.33333 18.6667H4.66667C4.025 18.6667 3.5 19.1917 3.5 19.8333C3.5 20.475 4.025 21 4.66667 21ZM3.5 8.16667C3.5 8.80833 4.025 9.33333 4.66667 9.33333H23.3333C23.975 9.33333 24.5 8.80833 24.5 8.16667C24.5 7.525 23.975 7 23.3333 7H4.66667C4.025 7 3.5 7.525 3.5 8.16667ZM4.66667 15.1667H16.3333C16.975 15.1667 17.5 14.6417 17.5 14C17.5 13.3583 16.975 12.8333 16.3333 12.8333H4.66667C4.025 12.8333 3.5 13.3583 3.5 14C3.5 14.6417 4.025 15.1667 4.66667 15.1667Z" fill="#0A5DAB"/>
+        </svg>
+    );
+};
+export const ArrowLongRight = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+        </svg>
+    )
+}
